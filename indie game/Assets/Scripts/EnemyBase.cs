a95430f14077c6740ec3a030b0d991339311acbc
Bart <kuijerlattie@gmail.com>
2015-11-18 11:48:28 +0100

    enemy basecommit
    first files for the enemy system, doesnt do much yet. committed to get
    some files from another commit

---
+++ indie game/Assets/Scripts/EnemyBase.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBase : MonoBehaviour {
+
+    protected float movespeed;
+    protected float hitpoints;
+    protected int difficulty;
+    protected GameObject target;
+
+	// Use this for initialization
+	void Start () {
+	   
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void Hit(float dmg)
+    {
+        hitpoints -= dmg;
+        if (hitpoints <= 0)
+        {
+            Die();
+        }
+    }
+
+    protected virtual void Die()
+    {
+        GameObject.Destroy(gameObject);
+    }
+}
---
+++ indie game/Assets/Scripts/EnemyMelee.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMelee : EnemyBase {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    protected override void Die()
+    {
+        base.Die();
+    }
+}

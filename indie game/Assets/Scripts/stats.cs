89e922f169c314baa5e0720969a57875bcafccf7
jossepr8 <jossepr8@hotmail.com>
2015-11-18 11:13:42 +0100

    added stats script

---
+++ indie game/Assets/Scripts/stats.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class stats : MonoBehaviour {
+
+    private int _health;
+    public int health { get { return _health; } set { _health = value > 0 ? (value <= maxhealth? value : maxhealth) : 0; } }
+    public int maxhealth;
+
+    private int _mana;
+    public int mana { get { return _mana; } set { _mana = value > 0 ? (value <= maxmana ? value : maxmana) : 0; } }
+    public int maxmana;
+
+
+    /*
+    int attack { get; set; }
+    int defense { get; set; }
+    int movespeed { get; set; }
+    int attackspeed { get; set; }
+    */
+}

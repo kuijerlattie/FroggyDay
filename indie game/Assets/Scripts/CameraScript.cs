a63b3d57b6c5d5bd2057ab1006e49d4123b65a8c
jossepr8 <jossepr8@hotmail.com>
2015-11-18 12:59:58 +0100

    new scene

---
+++ indie game/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+    [SerializeField]
+    Transform playerTransform;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = playerTransform.position;
+	}
+}

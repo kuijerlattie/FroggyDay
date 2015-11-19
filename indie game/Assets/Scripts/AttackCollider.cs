bbbed509f1c4e559b0766542d90f331f2048f966
jossepr8 <jossepr8@hotmail.com>
2015-11-19 01:25:23 +0100

    added basic spell
    :goat:

---
+++ indie game/Assets/Scripts/AttackCollider.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackCollider{
+
+	public static void MakeAttack(GameObject gameObject, Vector3 position, float radius, Transform attacker)
+    {
+       GameObject _gameObject = GameObject.Instantiate(gameObject);
+        //_gameObject.transform.SetParent(attacker);
+        _gameObject.transform.position = position;
+        _gameObject.GetComponent<SphereCollider>().radius = radius;
+        _gameObject.GetComponent<HitboxScript>().layer = attacker.gameObject.layer;
+        _gameObject.GetComponent<HitboxScript>().Spawn(0, 0);
+    }
+
+    public static void MakeSpell(GameObject gameObject, Vector3 position, float radius, Transform attacker)
+    {
+        Vector3 _forward = new Vector3(attacker.forward.x, attacker.forward.y, attacker.forward.z);
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject _gameObject = GameObject.Instantiate(gameObject);
+
+            _gameObject.transform.position = position + _forward * i;
+            _gameObject.GetComponent<SphereCollider>().radius = radius;
+            _gameObject.GetComponent<HitboxScript>().layer = attacker.gameObject.layer;
+            _gameObject.GetComponent<HitboxScript>().Spawn((float)i/10, 1);
+        }
+        
+    }
+
+
+
+}
--- indie game/Assets/Scripts/HitboxScript.cs
+++ indie game/Assets/Scripts/HitboxScript.cs
@@ -5,19 +5,43 @@ using System.Collections;
 public class HitboxScript : MonoBehaviour {
 
     SphereCollider _collider;
+    Collider[] colliders;
+    private float _duration = 0;
+    public int layer = 0;
 
-	public void Activated()
+    public void Spawn(float delay = 0.0f, float duration = 0.0f)
     {
+        _duration = duration;
+        StartCoroutine(StartSpawn(delay));
+    }
+
+    private IEnumerator StartSpawn(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        gameObject.GetComponent<MeshRenderer>().enabled = true;
         _collider = GetComponent<SphereCollider>();
-        Collider[] colliders = Physics.OverlapSphere(gameObject.transform.position, _collider.radius);
-        
-        foreach(Collider col in colliders)
+        colliders = Physics.OverlapSphere(gameObject.transform.position, _collider.radius);
+        CheckCollisions();
+
+    }
+    private void CheckCollisions()
+    {
+        foreach (Collider col in colliders)
         {
-          if(col.gameObject.layer != gameObject.layer)
+            if (col.gameObject.layer != layer)
             {
+                if (col.gameObject.GetComponent<stats>() != null) 
                 col.gameObject.GetComponent<stats>().Hit(1);
             }
         }
-        gameObject.SetActive(false);
+        StartCoroutine(DestroySelf());
     }
+
+    private IEnumerator DestroySelf()
+    {
+        gameObject.transform.parent = null;
+        yield return new WaitForSeconds(_duration);
+        GameObject.Destroy(this.gameObject);
+    }
+
 }

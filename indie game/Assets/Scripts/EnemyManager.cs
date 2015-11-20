cbb98e09435709d1f767c993a507d64d63119623
jossepr8 <jossepr8@hotmail.com>
2015-11-20 14:51:12 +0100

    added basic enemy spawner

--- indie game/Assets/Scripts/EnemyManager.cs
+++ indie game/Assets/Scripts/EnemyManager.cs
@@ -3,21 +3,36 @@ using System.Collections;
 
 public class EnemyManager : MonoBehaviour {
 
-    Transform[] spawnlocations;
+    GameObject[] spawnlocations;
     [SerializeField]
     GameObject meleeEnemyPrefab;
+    [SerializeField]
+    GameObject player;
+    float timer = 0;
 	// Use this for initialization
 	void Start () {
-	
+        spawnlocations = GameObject.FindGameObjectsWithTag("EnemySpawn");
 	}
 
     void SpawnEnemy()
     {
+        GameObject enemy = (GameObject)GameObject.Instantiate(meleeEnemyPrefab);
+        enemy.transform.position = GetSpawnLocation();
+    }
 
+    Vector3 GetSpawnLocation()
+    {
+        Vector3 location = spawnlocations[Random.Range(0, 2)].transform.position;
+        return location;
     }
 	
 	// Update is called once per frame
 	void Update () {
-	
+        timer += Time.deltaTime;
+        if(timer > 1)
+        {
+            SpawnEnemy();
+            timer = 0;
+        }
 	}
 }

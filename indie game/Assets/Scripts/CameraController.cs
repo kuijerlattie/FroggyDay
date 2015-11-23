15f9d1c9a8ac38242e725e3247fef65de474eff9
Bart <kuijerlattie@gmail.com>
2015-11-23 15:42:53 +0100

    level redone
    level redone

--- indie game/Assets/Scripts/CameraController.cs
+++ indie game/Assets/Scripts/CameraController.cs
@@ -6,9 +6,17 @@ public class CameraController : MonoBehaviour {
 
     float rotation;
     float rotationspeed = 90f;
+    float maxZoomDistance = 100f; //max distance away from player
+    float minZoomDistance = 10f; //min distance away from player
+    float zoomDistance = 100f;
+
+    RaycastHit hit;
+    GameObject player;
+    GameObject disabledWall;
+
 	// Use this for initialization
 	void Start () {
-	
+        player = GameObject.Find("Player");
 	}
 	
 	// Update is called once per frame
@@ -23,9 +31,42 @@ public class CameraController : MonoBehaviour {
         {
             rotation -= rotationspeed;
         }
+        zoomDistance += Input.GetAxis("Mouse ScrollWheel");
+        if (zoomDistance < minZoomDistance)
+            zoomDistance = minZoomDistance;
+        if (zoomDistance > maxZoomDistance)
+            zoomDistance = maxZoomDistance;
+
 
         rotation = rotation * Time.deltaTime;
 
         Camera.main.transform.RotateAround(transform.position, Vector3.up, rotation);
+
+        if (disabledWall != null)
+        {
+            disabledWall.SetActive(true);
+            disabledWall = null;
+        }
+        LayerMask layermask = (1 << 10);
+        //if (Physics.Raycast(Camera.main.ScreenPointToRay(player.transform.position), out hit, 100, layermask))
+        if (Physics.Raycast(player.transform.position, (Camera.main.transform.position - player.transform.position), out hit, 100, layermask))
+        {
+            Debug.Log("wall spotted");
+            //get distance from player to wall
+            //set camera at that distance
+        }
 	}
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+
 }
--- indie game/Assets/Scripts/VisionAssistant.cs
+++ indie game/Assets/Scripts/VisionAssistant.cs
@@ -3,30 +3,5 @@ using System.Collections;
 
 public class VisionAssistant : MonoBehaviour {
 
-    RaycastHit hit;
-    GameObject player;
-    GameObject disabledWall;
-
-	// Use this for initialization
-	void Start () {
-        player = GameObject.Find("Player");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-        if (disabledWall != null)
-        {
-            disabledWall.SetActive(true);
-            disabledWall = null;
-        }
-        LayerMask layermask = (1 << 10);
-        //if (Physics.Raycast(Camera.main.ScreenPointToRay(player.transform.position), out hit, 100, layermask))
-        if (Physics.Raycast(Camera.main.transform.position, (player.transform.position - Camera.main.transform.position), out hit, 100, layermask))
-        {
-            Debug.Log("wall spotted");
-            hit.collider.gameObject.transform.parent.gameObject.SetActive(false);
-            disabledWall = hit.collider.gameObject.transform.parent.gameObject;
-        }
-	}
+    
 }

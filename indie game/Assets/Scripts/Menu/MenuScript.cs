da4d194af1e920ec4c5239bd59786e93674d6aeb
Bart <kuijerlattie@gmail.com>
2015-11-27 00:57:23 +0100

    Consuela says NO
    cleaned up the project, 10 scenes with random names in root folder is
    over... rip in peperonies :meat_on_bone:

---
+++ indie game/Assets/Scripts/Menu/MenuScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Play()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}

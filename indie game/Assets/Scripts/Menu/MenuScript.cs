a0c1d5ffe7ea2ac4b5d89781b82e187487308515
Baspower <BGunnink@hotmail.com>
2015-11-27 11:35:31 +0100

    Greybox
    for sprintmeeting

--- indie game/Assets/Scripts/Menu/MenuScript.cs
+++ indie game/Assets/Scripts/Menu/MenuScript.cs
@@ -15,7 +15,7 @@ public class MenuScript : MonoBehaviour {
 
     public void Play()
     {
-        Application.LoadLevel(1);
+        Application.LoadLevel("mninimapscene");
     }
 
     public void Quit()

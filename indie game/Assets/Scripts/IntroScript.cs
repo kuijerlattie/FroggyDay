536b1c4d05fa2025421bd647e595391817305046
Bart <kuijerlattie@gmail.com>
2015-12-01 17:08:07 +0100

    changed text in tutorial, made ending a coroutine

--- indie game/Assets/Scripts/IntroScript.cs
+++ indie game/Assets/Scripts/IntroScript.cs
@@ -39,7 +39,6 @@ public class IntroScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         //check tutorial break here
-        Debug.Log("updateing intro shizzle");
         if (Input.GetKeyDown(KeyCode.F))
         {
             SingSong();
@@ -80,7 +79,7 @@ public class IntroScript : MonoBehaviour {
             if (potionInstruction && CheckPotion())
             {
                 hasUsedPotion = true;
-                EndTutorial();
+                StartCoroutine(EndTutorial());
             }
         }
 	}
@@ -88,12 +87,9 @@ public class IntroScript : MonoBehaviour {
     IEnumerator StartTutorial()
     {
         Debug.Log("StartedTutorial");
-        ShowText("I no speak london, cyka blyet");
-        yield return new WaitForSeconds(WaitTimeInSeconds);
-        ShowText("Hello teddybear, Walk to the target!");
+        ShowText("Hello Jian, we will prepare your journey today, to become the master! \r\n Walk to the target that just appeared Jian!");
         yield return new WaitForSeconds(WaitTimeInSeconds);
         WalkTarget.GetComponent<MeshRenderer>().enabled = true;//turn on walk target
-        Debug.Log("finished start tutorial");
         walkedInstructions = true;
         yield return new WaitForSeconds(0.01f);
         
@@ -113,18 +109,15 @@ public class IntroScript : MonoBehaviour {
     IEnumerator StartRotation()
     {
         playerrotationstart = FindObjectOfType<CameraController>().rotation;
-        ShowText("Great, you know walking. Now spinbot!");
+        ShowText("Now rotate the camera around yourself Jian. you can do this by pressing A or D");
         //say rotation text
-        yield return new WaitForSeconds(WaitTimeInSeconds);
-        ShowText("Did you know you can also press the f button to make me sing American rap song?");
-        //set rotation target
+        yield return new WaitForSeconds(WaitTimeInSeconds);//set rotation target
         rotateInstructions = true;
         yield return new WaitForSeconds(0.01f);
     }
 
     bool CheckRotation()
     {
-        Debug.Log("player start rotation: " + playerrotationstart + "player current rotation: " + FindObjectOfType<CameraController>().rotation);
         if (playerrotationstart < FindObjectOfType<CameraController>().rotation || playerrotationstart > FindObjectOfType<CameraController>().rotation)
             return true;
         return false;
@@ -132,10 +125,7 @@ public class IntroScript : MonoBehaviour {
 
     IEnumerator StartEnemy()
     {
-        ShowText("You are real spinbot master. Now it is time to learn how to attack.");
-        //say enemy text
-        yield return new WaitForSeconds(WaitTimeInSeconds);
-        ShowText("I shall summon you a teddybear. Press q to hit it with your spell. dont forget to aim at the teddybear by putting your mouse cursor on him!");
+        ShowText("Look at the Spellbar at the bottom of your screen Jian. Your first spell will pop up in there now");
         player.SetSpell(PlayerScript.SpellSlots.spellQ, 1);
         //set enemy target;
         yield return new WaitForSeconds(2f);
@@ -148,7 +138,7 @@ public class IntroScript : MonoBehaviour {
         yield return new WaitForSeconds(2f);
         enemy.GetComponent<EnemyBase>().Stun(600);
         enemyInstructions = true;
-        ShowText("I stunned the teddybear so he cannot harm you. Press q to hit it with your spell. dont forget to aim at the teddybear by putting your mouse cursor on him!");
+        ShowText("An enemy appeared Jian! Kill him by aiming at him with your mouse and then pressing Q to cast a spell");
 
         yield return new WaitForSeconds(0.01f);
     }
@@ -166,7 +156,7 @@ public class IntroScript : MonoBehaviour {
     {
         player.Hit(10); //hit player to make sure he is not at full health anymore
         //say potion text
-        ShowText("If you lose health when fighting enemies, you can use a health potion to heal yourself. Pick up the health potion that i just spawned for you, then press 1 to drink it.");
+        ShowText("If you lose health when fighting enemies, you can use a health potion to heal yourself. \r\n Pick up the health potion that i just spawned for you, then press 1 to drink it.");
         Healingpotion hppot = new Healingpotion();
         hppot.healingValue = 100;
         hppot.Drop(HealthpotionSpawn.transform.position);
@@ -176,7 +166,6 @@ public class IntroScript : MonoBehaviour {
 
     bool CheckPotion()
     {
-        Debug.Log("Checking player health");
         if (player.health >= 100) //cheap way to check if he used the healthpot. remember that we damage him before spawning a healthpot to make sure he isnt on 100% health anymore when we check this
         {
             return true;
@@ -184,9 +173,12 @@ public class IntroScript : MonoBehaviour {
         return false;
     }
 
-    void EndTutorial()
+    IEnumerator EndTutorial()
     {
-        ShowText("Thats it, tutorial is over. plz go away now!");
+        ShowText("I will be in the yard if you neeed me for further guidance.");
+        yield return new WaitForSeconds(10);
+        GameObject.Destroy(gameObject);
+
     }
 
     void ShowText(string ptext)

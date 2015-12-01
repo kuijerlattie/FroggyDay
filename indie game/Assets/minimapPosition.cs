32f5fbf18e1082608097f8650000f67f217c6850
jossepr8 <jossepr8@hotmail.com>
2015-12-01 10:59:52 +0100

    minimapoverlayscript

---
+++ indie game/Assets/minimapPosition.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class minimapPosition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 pos = new Vector2(Screen.width - 250, Screen.height - 250); //minimap position 
+        GetComponent<Image>().rectTransform.position = pos;
+        GetComponent<Image>().rectTransform.pivot = Vector2.zero;
+    }
+}

ac2db27deb10b701059006d938d9de7c083d5174
Bart <kuijerlattie@gmail.com>
2015-12-03 01:21:36 +0100

    tried to make triggers work for starting new waves

---
+++ indie game/Assets/Scripts/Waves/ActivateWaveScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateWaveScript : MonoBehaviour {
+
+    public int AreaNumber = 1;
+
+    EnemyManager manager;
+    bool active = true;
+
+	// Use this for initialization
+	void Start () {
+        manager = GameObject.FindObjectOfType<EnemyManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (active)
+        {
+            Debug.Log("new wave started");
+            manager.StartWave(AreaNumber);
+            active = false;
+        }
+    }
+
+    public void Reset()
+    {
+        active = true;
+    }
+}
---
+++ indie game/Assets/Scripts/Waves/ResetTriggerScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetTriggerScript : MonoBehaviour {
+
+    public GameObject waveActivator;
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("wavestarter reset!");
+        waveActivator.GetComponent<ActivateWaveScript>().Reset();
+    }
+}
